4ecf56046e86a60b568243bb0b706cec01b5dbc3
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-09-17 00:11:01 +0300

    Load and Store values in Memory

--- Chip8/Memory.hs
+++ Chip8/Memory.hs
@@ -1,7 +1,8 @@
 module Chip8.Memory where
 
 import Data.Word (Word8, Word16)
-import Data.Array.ST (STUArray, newArray)
+import Data.STRef
+import Data.Array.ST (STUArray, newArray, readArray, writeArray)
 import Control.Monad.ST (ST)
 
 data Address = Register Register
@@ -13,17 +14,18 @@ data Register = V1 | V2 | V3 | V4 | V5
               | V6 | V7 | V8 | V9 | VA
               | VB | VC | VD | VE | VF
               | DT | ST | I
+              deriving (Enum)
 
 data MemoryValue = MemoryValue8 Word8
                  | MemoryValue16 Word16
 
 data Memory s = Memory { memory :: STUArray s Word16 Word8
                        , registers :: STUArray s Word8 Word8
-                       , registerI :: Word16
-                       , delayTimer :: Word8
-                       , soundTimer :: Word8
-                       , pc :: Word16
-                       , sp :: Word8
+                       , registerI :: STRef s Word16
+                       , delayTimer :: STRef s Word8
+                       , soundTimer :: STRef s Word8
+                       , pc :: STRef s Word16
+                       , sp :: STRef s Word8
                        , stack :: STUArray s Word8 Word16
                        }
 
@@ -31,22 +33,40 @@ new :: ST s (Memory s)
 new = do
     memory' <- newArray (0x000, 0xFFF) 0
     registers' <- newArray (0x0, 0xF) 0
+    registerI' <- newSTRef 0
+    delayTimer' <- newSTRef 0
+    soundTimer' <- newSTRef 0
+    pc' <- newSTRef 0x200
+    sp' <- newSTRef 0
     stack' <- newArray (0x0, 0xF) 0
     return Memory { memory = memory'
                   , registers = registers'
-                  , registerI = 0
-                  , delayTimer = 0
-                  , soundTimer = 0
-                  , pc = 0x200
-                  , sp = 0
+                  , registerI = registerI'
+                  , delayTimer = delayTimer'
+                  , soundTimer = soundTimer'
+                  , pc = pc'
+                  , sp = sp'
                   , stack = stack'
                   }
 
 store :: Memory s -> Address -> MemoryValue -> ST s ()
-store = undefined
+store mem Pc            (MemoryValue16 v) = writeSTRef (pc mem) v
+store mem Sp            (MemoryValue8 v)  = writeSTRef (sp mem) v
+store mem (Register I)  (MemoryValue16 v) = writeSTRef (registerI mem) v
+store mem (Register DT) (MemoryValue8 v)  = writeSTRef (delayTimer mem) v
+store mem (Register ST) (MemoryValue8 v)  = writeSTRef (soundTimer mem) v
+store mem (Register r)  (MemoryValue8 v)  = writeArray (registers mem) (fromIntegral $ fromEnum r) v
+store mem (Ram r)       (MemoryValue8 v)  = writeArray (memory mem) r v
+store _ _ _ = error "Incorrect Memory 'store' command"
 
 load :: Memory s -> Address -> ST s MemoryValue
-load = undefined
+load mem Pc            = readSTRef (pc mem) >>= \v -> return $ MemoryValue16 v
+load mem Sp            = readSTRef (sp mem) >>= \v -> return $ MemoryValue8 v
+load mem (Register I)  = readSTRef (registerI mem) >>= \v -> return $ MemoryValue16 v
+load mem (Register DT) = readSTRef (delayTimer mem) >>= \v -> return $ MemoryValue8 v
+load mem (Register ST) = readSTRef (soundTimer mem) >>= \v -> return $ MemoryValue8 v
+load mem (Register r)  = readArray (registers mem) (fromIntegral $ fromEnum r) >>= \v -> return $ MemoryValue8 v
+load mem (Ram r)       = readArray (memory mem) r >>= \v -> return $ MemoryValue8 v
 
 font :: [Word8]
 font = [ 0xF0, 0x90, 0x90, 0x90, 0xF0 -- 0
