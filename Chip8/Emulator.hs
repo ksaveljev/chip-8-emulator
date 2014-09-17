11f3b7eef94ef2cc237f1515b2dcfee6b7e71888
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-09-17 14:43:09 +0300

    Read rom file and load it into memory

--- Chip8/Emulator.hs
+++ Chip8/Emulator.hs
@@ -3,6 +3,14 @@ module Chip8.Emulator where
 import qualified Data.ByteString as B
 
 import Chip8.Monad
+import Chip8.Memory (Address(..), MemoryValue(..))
 
 loadProgram :: MonadEmulator m => B.ByteString -> m ()
-loadProgram = undefined
+loadProgram program =
+    mapM_ loadByte [0..B.length program - 1]
+    where
+      offset = 0x200
+      loadByte i = 
+        let byte = B.index program i
+            address = offset + fromIntegral i
+        in store (Ram address) (MemoryValue8 byte)
