ec4665f00032099bf782923b893019531d0ee95d
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-09-18 14:57:11 +0300

    Oops, forgot the V0 register

--- Chip8/Memory.hs
+++ Chip8/Memory.hs
@@ -10,9 +10,10 @@ data Address = Register Register
              | Sp
              | Ram Word16
 
-data Register = V1 | V2 | V3 | V4 | V5
-              | V6 | V7 | V8 | V9 | VA
-              | VB | VC | VD | VE | VF
+data Register = V0 | V1 | V2 | V3
+              | V4 | V5 | V6 | V7
+              | V8 | V9 | VA | VB
+              | VC | VD | VE | VF
               | DT | ST | I
               deriving (Enum)
 
