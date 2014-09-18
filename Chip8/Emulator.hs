b3dfd595bcd6c7a0b998fb559eef326c13442cc3
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-09-18 12:44:31 +0300

    Load instruction

--- Chip8/Emulator.hs
+++ Chip8/Emulator.hs
@@ -1,9 +1,13 @@
 module Chip8.Emulator where
 
+import Data.Word (Word16)
+import Data.Bits (shiftL)
+import Control.Applicative ((<$>))
 import qualified Data.ByteString as B
 
 import Chip8.Monad
 import Chip8.Memory (Address(..), MemoryValue(..))
+import Chip8.Instruction
 
 loadProgram :: MonadEmulator m => B.ByteString -> m ()
 loadProgram program =
@@ -14,3 +18,14 @@ loadProgram program =
         let byte = B.index program i
             address = offset + fromIntegral i
         in store (Ram address) (MemoryValue8 byte)
+
+loadNextWord :: MonadEmulator m => m Word16
+loadNextWord = do
+    (MemoryValue16 pc) <- load Pc
+    (MemoryValue8 msbyte) <- load (Ram pc)
+    (MemoryValue8 lsbyte) <- load (Ram $ pc + 1)
+    store Pc $ MemoryValue16 (pc + 2)
+    return $ fromIntegral (msbyte `shiftL` 8) + fromIntegral lsbyte
+
+loadInstruction :: MonadEmulator m => m Instruction
+loadInstruction = decodeInstruction <$> loadNextWord
